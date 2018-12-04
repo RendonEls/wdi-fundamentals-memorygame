ed2f7f9b62ed7f9da38508494008dde8413d71ae
Rendon Elsesser <rendonelsesser@gmail.com>
2018-12-04 15:53:58 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,34 +32,25 @@ var checkForMatch = function(){
 			alert("Sorry, try again.");
 	}
 };
-
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch()
+	}
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	
-	if (cardsInPlay.length === 2);
-	checkForMatch();
+};
+var createBoard = function() {
+	for (i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 
-
-}
-
-flipCard(0);
-flipCard(2);
-
-
-//console.log(cardsInPlay);
-//console.log(cardOne);
-
-//console.log("User flipped " + cardTwo);
-
-/*
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-*/
+	}
+};
+createBoard();
